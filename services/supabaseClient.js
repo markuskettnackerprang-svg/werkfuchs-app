f068f23c597a25650b1bf1799ef4ccba1f92516e
markuskettnackerprang-svg <markus.kettnacker.prang@gmail.com>
2026-04-28 06:50:56 +0000

    setup supabase client

---
+++ services/supabaseClient.js
@@ -0,0 +1,7 @@
+import "react-native-url-polyfill/auto";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://talhdevjxffrysmmqwhi.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhbGhkZXZqeGZmcnlzbW1xd2hpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczMjg1MDYsImV4cCI6MjA5MjkwNDUwNn0.tLVlpgdxw5nOeJOzxYoXE2SijD8on0PlQZFOG7v2NZg";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
